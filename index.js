fc266e493d7e36e25f32b9a9928eae40f89135c4
Eric F <ejfullmer@hotmail.com>
2020-03-28 21:53:53 -0600

    TEST

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const http = require('http');
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const randomColor = require('randomcolor');
+const express = require('express')
+const path = require('path')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const PORT = process.env.PORT || 5000;
-const INDEX = path.resolve(__dirname, '../index.html');
+app.set('port', (process.env.PORT || 5000));
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-const brushes = [];
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.sendFile(INDEX));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-io.on('connection', (socket) => {
-  let brush = { color: randomColor({ luminosity: 'bright' }), strokes: [] }
-  brushes.push(brush);
+console.log("outside io");
 
-  socket.on('strokes', (strokes) => {
-    brush.strokes.push(...strokes);
-    io.emit('paint', brush.color, strokes);
-  })
+io.on('connection', function(socket){
 
-  socket.on('refresh', () => {
-    brushes.forEach(b => socket.emit('paint', b.color, b.strokes));
-  })
-})
+  console.log('User Conncetion');
 
-server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  socket.on('connect user', function(user){
+    console.log("Connected user ");
+    io.emit('connect user', user);
+  });
+
+  socket.on('on typing', function(typing){
+    console.log("Typing.... ");
+    io.emit('on typing', typing);
+  });
+
+  socket.on('chat message', function(msg){
+    console.log("Message " + msg['message']);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
