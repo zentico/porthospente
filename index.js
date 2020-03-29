de2bb4c483276411f8f5804c8366c3fbbd694d34
Eric F <ejfullmer@hotmail.com>
2020-03-29 00:13:39 -0600

    move to pente!

--- index.js
+++ index.js
@@ -1,57 +1,133 @@
+var adler32 = require('./lib/adler32');
+var Game = require('./lib/game');
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-
-var Colours = [], Users = [];
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html')
 })
 
-app.get('/styles.css', function(req, res) {
-  res.sendFile(__dirname + '/client/styles.css')
+app.get('/chat.js', function(req, res) {
+  res.sendFile(__dirname + '/client/pente_client.js')
 })
 
 app.get('/chat.js', function(req, res) {
-  res.sendFile(__dirname + '/client/chat.js')
+  res.sendFile(__dirname + '/client/pente.css')
 })
 
-io.on('connection', function(socket){
-  UserCheck()
-  console.log('a user connected')
-  io.emit('chat message', '<strong style="color:red">A user connected</strong>')
-  socket.on('disconnect', function(){
-    UserCheck()
-    console.log('user disconnected')
-    io.emit('chat message', '<strong style="color:blue">A user disconnected</strong>')
-  })
-  socket.on('chat message', function(msg){
-    UserCheck()
-    if (msg.message == '') {
-      return
-    }
-    if (!Colours[msg.user]) {
-      var col = '#'+Math.floor(Math.random()*16777215).toString(16)
-      Colours[msg.user] = col
-    }
-    msg.message = msg.message.replace(/script/igm, 'scrip_t').replace(/on/igm, 'o_n')
-    console.log('message: ' + msg.message + '(' + msg.user + ')')
-    io.emit('chat message', '<strong style="color:' + Colours[msg.user] + '">' + msg.user + '</strong> ' + msg.message)
-  })
-  socket.on('user active', function (msg) {
-    Users.push('<span>' + msg + '</span>')
-  })
+app.get('/chat.js', function(req, res) {
+  res.sendFile(__dirname + '/client/reset.css')
 })
 
-function UserCheck() {
-  Users = []
-  // console.log('Checking users');
-  io.emit('check users', '')
-  setTimeout(function () {
-    // console.log('Updating users');
-    io.emit('users update', Users)
-  }, 2000)
+app.get('/fonts/:fm/:ft', function(req, res) {
+  res.sendFile(__dirname + '/client/fonts/' + req.params.fm + '/' + req.params.ft)
+})
+
+var ALLGAMES = {}; //
+
+/*****************
+    Game State
+*****************/
+
+function gameState(hash) {
+	this.hash = hash;
+	this.board = [];
+	this.gameStack = [];
+	this.turnStack = [];
+	this.score = {0:0, 1:0};
+	this.gameOver = false;
+	this.thisPlayer = 0;
+	this.alreadyPlayed = false;
 }
+
+/***************************
+    Socket Communication
+***************************/
+
+// On connection to the client
+io.sockets.on('connection', function(socket){
+	log('client connected');
+	// Let the client know that the server is ready
+	socket.emit('serverReady');
+	// Socket hasn't joined a game
+	socket.inGame = false;
+
+	// On request for a new game
+	socket.on('newBoard', function(board){
+		log('New game requested');
+
+		// Use the current time and date as the input to the hash
+		var hashInput = new Date();
+		hashInput = hashInput.toISOString();
+		// Generate hash
+		var hash = adler32(hashInput);
+		// Prevent collision with an existing hash
+		while(ALLGAMES.hasOwnProperty(hash)){
+			hashInput = new Date();
+			hashInput = hashInput.toISOString();
+			hash = adler32(hashInput);
+		}
+		// Add a new game to the ALLGAMES hash table
+		ALLGAMES[hash] = new gameState(hash);
+		// Store the hash in the socket
+		socket.gameCode = hash;
+		socket.inGame = true;
+		socket.player = 0;
+		log("Hash is "+socket.gameCode);
+		// Join the room with that hash
+		socket.join(socket.gameCode);
+		Game.init(board.xBoardSize, board.yBoardSize, ALLGAMES[socket.gameCode]);
+		socket.emit('joinedGame', socket.gameCode);
+		io.sockets.in(socket.gameCode).emit('render',ALLGAMES[socket.gameCode]);
+	});
+
+	socket.on('joinGame', function(hash){
+		log("Received hash: "+hash);
+		if(ALLGAMES.hasOwnProperty(hash)){
+			socket.gameCode = hash;
+			socket.inGame = true;
+			socket.player = 1;
+			socket.join(socket.gameCode);
+			log("Sending joinedGame");
+			socket.emit('joinedGame', socket.gameCode);
+			io.sockets.in(socket.gameCode).emit('render',ALLGAMES[socket.gameCode]);
+		} else {
+			log("hash doesn't exist");
+			socket.emit('doesNotExist', hash);
+		}
+	});
+
+	// On request to play a postion
+	socket.on('play', function(position){
+		// If in a game
+		if(socket.inGame){
+			// And if it's that player's turn
+			if(socket.player === ALLGAMES[socket.gameCode].thisPlayer){
+				log('Play at '+position.xPos+", "+position.yPos);
+				Game.play(position.xPos, position.yPos, ALLGAMES[socket.gameCode]);
+				io.sockets.in(socket.gameCode).emit('render',ALLGAMES[socket.gameCode]);
+			}
+		}
+	});
+
+	// On request to undo
+	socket.on('undo', function(data){
+		Game.undo(ALLGAMES[socket.gameCode]);
+		io.sockets.in(socket.gameCode).emit('render',ALLGAMES[socket.gameCode]);
+	});
+
+	// On request to end turn
+	socket.on('endTurn', function(){
+		if(socket.player === ALLGAMES[socket.gameCode].thisPlayer){
+			ALLGAMES[socket.gameCode].alreadyPlayed = false;
+      Game.switchPlayer(ALLGAMES[socket.gameCode]);
+			Game.newTurn(ALLGAMES[socket.gameCode]);
+			io.sockets.in(socket.gameCode).emit('render',ALLGAMES[socket.gameCode]);
+		}
+	});
+
+});
 
 http.listen(process.env.PORT || 80, function(){
   console.log('listening on *:3000')
