14f4d2ee6b7a63df1e6ae0336635ee21e3a190d3
Eric F <ejfullmer@hotmail.com>
2020-03-28 21:35:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const http = require('http')
-const express = require('express')
-const socketIO = require('socket.io')
-const path = require('path')
-const randomColor = require('randomcolor')
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const randomColor = require('randomcolor');
 
-const PORT = process.env.PORT || 5000
-const INDEX = path.resolve(__dirname, '../index.html')
+const PORT = process.env.PORT || 5000;
+const INDEX = path.resolve(__dirname, '../index.html');
 
-const app = express()
-const server = http.createServer(app)
-const io = socketIO(server)
-const brushes = []
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const brushes = [];
 
-app.get('/', (req, res) => res.sendFile(INDEX))
+app.get('/', (req, res) => res.sendFile(INDEX));
 
 io.on('connection', (socket) => {
   let brush = { color: randomColor({ luminosity: 'bright' }), strokes: [] }
-  brushes.push(brush)
+  brushes.push(brush);
 
   socket.on('strokes', (strokes) => {
-    brush.strokes.push(...strokes)
-    io.emit('paint', brush.color, strokes)
+    brush.strokes.push(...strokes);
+    io.emit('paint', brush.color, strokes);
   })
 
   socket.on('refresh', () => {
-    brushes.forEach(b => socket.emit('paint', b.color, b.strokes))
+    brushes.forEach(b => socket.emit('paint', b.color, b.strokes));
   })
 })
 
-server.listen(PORT, () => console.log(`Listening on ${PORT}`))
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
